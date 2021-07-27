0edd4f3fb37b6b1ca15e49b6071d07a035ac258f
Terry Chia <chiaweizhengterry@MacBook-Pro-2.local>
2021-07-27 11:37:05 +0800

    chore: add configuration type for presentation

--- WWDC2021/WWDC2021/Buttons/ButtonWithConfigurationViewController.swift
+++ WWDC2021/WWDC2021/Buttons/ButtonWithConfigurationViewController.swift
@@ -20,7 +20,13 @@ struct ButtonWithConfigurationViewControllerRepresentable: UIViewControllerRepre
 
 class ButtonWithConfigurationViewController: UIViewController {
     var firstButton: UIButton!
-
+    
+    //configuration
+    var grayButton: UIButton!
+    var plainButton: UIButton!
+    var filledButton: UIButton!
+    var tinitedButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -31,7 +37,12 @@ class ButtonWithConfigurationViewController: UIViewController {
 extension ButtonWithConfigurationViewController {
     private func setupViewController() {
         setupFirstButton()
-
+        
+        setupGrayButton()
+        setupPlainButton()
+        setupFilledButton()
+        setupTintedButton()
+        
         setupConstraints()
     }
 
@@ -46,6 +57,35 @@ extension ButtonWithConfigurationViewController {
 
         firstButton = UIButton(configuration: configuration)
     }
+    
+    private func setupGrayButton() {
+        var configuration = UIButton.Configuration.gray()
+        configuration.title = String(localized: "Gray")
+        
+        grayButton = UIButton(configuration: configuration)
+    }
+    
+    private func setupPlainButton() {
+        var configuration = UIButton.Configuration.plain()
+        configuration.title = String(localized: "Plain")
+        
+        plainButton = UIButton(configuration: configuration)
+    }
+    
+    private func setupFilledButton() {
+        var configuration = UIButton.Configuration.filled()
+        configuration.title = String(localized: "filled")
+        
+        filledButton = UIButton(configuration: configuration)
+    }
+    
+    private func setupTintedButton() {
+        var configuration = UIButton.Configuration.tinted()
+        configuration.title = String(localized: "Tinted")
+        
+        tinitedButton = UIButton(configuration: configuration)
+    }
+    
 
     private func setupConstraints() {
         if firstButton != nil {
@@ -59,5 +99,53 @@ extension ButtonWithConfigurationViewController {
 
             NSLayoutConstraint.activate(constraints)
         }
+        
+        if grayButton != nil {
+            view.addSubview(grayButton)
+            
+            let contraints = [
+                grayButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 200.0),
+                grayButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40)
+            ]
+            grayButton.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate(contraints)
+        }
+        
+        if filledButton != nil {
+            view.addSubview(filledButton)
+            
+            let contraints = [
+                filledButton.topAnchor.constraint(equalTo: grayButton.topAnchor),
+                filledButton.leadingAnchor.constraint(equalTo: grayButton.trailingAnchor,constant: 20)
+            ]
+            
+            filledButton.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate(contraints)
+        }
+        
+        if plainButton != nil {
+            view.addSubview(plainButton)
+
+            let constraints = [
+                plainButton.topAnchor.constraint(equalTo: grayButton.topAnchor),
+                plainButton.leadingAnchor.constraint(equalTo: filledButton.trailingAnchor,constant: 20)
+            ]
+
+            plainButton.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate(constraints)
+        }
+        
+        if tinitedButton != nil {
+            view.addSubview(tinitedButton)
+
+            let constraints = [
+                tinitedButton.topAnchor.constraint(equalTo: grayButton.topAnchor),
+                tinitedButton.leadingAnchor.constraint(equalTo: plainButton.trailingAnchor, constant: 20)
+            ]
+
+            tinitedButton.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate(constraints)
+        }
+        
     }
 }
